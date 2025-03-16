62f97baebf0c7e932db61d5c11dcab191592349c
Jimmy Luong <164375144+github-jimjim@users.noreply.github.com>
2025-03-16 16:38:07 +0100

    Update example_2.py

--- nnue_probe_rs/example_2.py
+++ nnue_probe_rs/example_2.py
@@ -1,4 +1,5 @@
-import pynnue
-pynnue.init_nnue("final.jnn")
-wert = pynnue.eval_nnue("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+import nnue_parser
+nnue_parser.init_nnue_py("final.jnn")
+wert = nnue_parser.eval_nnue_py("rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1")
 print("Bewertung in Centipawns:", wert)
+input()
